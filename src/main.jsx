48fe3e77f4c96b2c66abf0e3708dd418e1bd980c
Muhammad Naufal <muh.naufal2103@gmail.com>
2025-12-08 16:05:43 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,18 @@ import './index.css';
 import App from './App.jsx'; // Landing Page
 import Main from './Pages/MainLayout.jsx'; // Halaman Utama
 import Splash from './Pages/Splash.jsx'; // Splash Screen
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Projects from './Pages/Projects.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Portfolio">
+    <HashRouter basename="/Portfolio">
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/landing" element={<App />} />
         <Route path="/main" element={<Main />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );

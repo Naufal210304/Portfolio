64029c85507476f387f41c59feef68aec58c8324
Muhammad Naufal <muh.naufal2103@gmail.com>
2025-12-08 16:13:07 +0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Projects from './Pages/Projects.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/Portfolio">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/landing" element={<App />} />
